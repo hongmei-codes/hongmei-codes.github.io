c678a53cc123331b7fa3e3d72af0db4521bef031
hongmei <53332372+hongmei-codes@users.noreply.github.com>
2023-07-08 20:09:44 +0800

    basic header main layout

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,16 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <code>coming soon</code>
-      </div>
-    </main>
+    <div className="lg:flex lg:justify-between lg:gap-4">
+      <header>
+        <h1 className="text-4xl font-bold text-slate-200">hongmei</h1>
+        <h3>I like to build apps that live on the web</h3>
+      </header>
+      <main>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi repellat
+        ipsa dolorem. Quo consequatur iste odio veritatis tempore natus magni at
+        fuga ad adipisci, quasi libero, veniam tenetur itaque soluta!
+      </main>
+    </div>
   );
 }
