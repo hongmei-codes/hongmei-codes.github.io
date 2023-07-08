1a59b693aa53cdfb5d1f0be860ca9e99e0cb693c
hongmei <53332372+hongmei-codes@users.noreply.github.com>
2023-07-08 20:24:55 +0800

    small text change

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,9 @@ export default function Home() {
     <div className="lg:flex lg:justify-between lg:gap-4">
       <header>
         <h1 className="text-4xl font-bold text-slate-200">hongmei</h1>
-        <h3>I like to build apps that live on the web</h3>
+        <h3 className="font-bold text-slate-200">
+          I like to build apps that live on the web
+        </h3>
       </header>
       <main>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi repellat
