23da17fea322e81b29f60872d57adce0f6578ae0
hongmei <53332372+hongmei-codes@users.noreply.github.com>
2023-07-10 20:26:19 +0800

    small text change

--- app/page.tsx
+++ app/page.tsx
@@ -2,18 +2,14 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="lg:flex lg:justify-between lg:gap-4">
+    <div className="dark:bg-black lg:flex lg:justify-between lg:gap-4">
       <header>
         <h1 className="text-4xl font-bold text-slate-200">hongmei</h1>
         <h3 className="font-bold text-slate-200">
           I like to build apps that live on the web
         </h3>
       </header>
-      <main>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi repellat
-        ipsa dolorem. Quo consequatur iste odio veritatis tempore natus magni at
-        fuga ad adipisci, quasi libero, veniam tenetur itaque soluta!
-      </main>
+      <main className="dark:text-white"></main>
     </div>
   );
 }
