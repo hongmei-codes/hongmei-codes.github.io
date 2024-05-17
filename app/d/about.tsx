337963a71faa697ab0b5b788f8d00800ff635379
hongmei <53332372+hongmei-codes@users.noreply.github.com>
2024-05-18 00:00:08 +0800

    copy components

---
+++ app/d/about.tsx
@@ -0,0 +1,57 @@
+export default function About() {
+  return (
+    <section id="about" className="mb-16">
+      <div className="sticky top-0 z-20 backdrop-blur py-4 -mx-10 lg:-mx-24 md:-mx-24 px-10 lg:px-24 md:px-24">
+        <h2 className="dark:text-slate-200 text-slate-600 font-bold">ABOUT</h2>
+      </div>
+      <p className="mb-4">
+        I started learning{" "}
+        <a
+          href="https://www.w3schools.com/js/"
+          target="_blank"
+          className="font-medium dark:text-slate-200 text-slate-600 hover:text-primary dark:hover:text-primary"
+        >
+          JavaScript
+        </a>{" "}
+        back when we were using{" "}
+        <span className="font-medium dark:text-slate-200 text-slate-600 hover:text-primary dark:hover:text-primary lg:cursor-[url('/ie.png')_15_15,_pointer]">
+          Internet Explorer.
+        </span>{" "}
+      </p>
+      <p className="mb-4">
+        Currently, I work with start-ups striving to make a positive impact in
+        the world. I’ve had the pleasure of contributing to meaningful missions
+        like{" "}
+        <a
+          href="https://www.stemly.ai/"
+          target="_blank"
+          className="font-medium dark:text-slate-200 text-slate-600 hover:text-primary dark:hover:text-primary"
+        >
+          reducing waste in supply chains
+        </a>
+        , and{" "}
+        <a
+          href="https://www.rimm.io/"
+          target="_blank"
+          className="font-medium dark:text-slate-200 text-slate-600 hover:text-primary dark:hover:text-primary"
+        >
+          democratizing sustainability
+        </a>
+        .
+      </p>
+      <p className="mb-4">
+        I am always trying to write better code and build cooler apps! Now, I am
+        expanding my horizon by learning to build apps that live on phones.
+        Whether you want to work with me or just to say hi, feel free to{" "}
+        <a
+          href="mailto:duhongmei@pm.me"
+          target="_blank"
+          className="font-medium dark:text-slate-200 text-slate-600 hover:text-primary dark:hover:text-primary"
+        >
+          get in touch
+        </a>
+        !
+      </p>
+    </section>
+  );
+}
---
+++ app/d/skills.tsx
@@ -0,0 +1,17 @@
+import Skill from "./skill";
+import { skills } from "../contents";
+
+export default function Skills() {
+  return (
+    <section id="skills" className="mb-16">
+      <div className="sticky top-0 z-20 backdrop-blur py-4 -mx-10 lg:-mx-24 md:-mx-24 px-10 lg:px-24 md:px-24">
+        <h2 className="dark:text-slate-200 text-slate-600 font-bold">SKILLS</h2>
+      </div>
+      <div className="grid gap-3 lg:gap-6 md:gap-4 lg:grid-cols-4 md:grid-cols-2 grid-cols-2 ">
+        {skills.map((skill, i) => (
+          <Skill skill={skill} key={i} />
+        ))}
+      </div>
+    </section>
+  );
+}
