68fbb38ba9dd953c38a59fd6248ac1f072ebd719
hongmei <53332372+hongmei-codes@users.noreply.github.com>
2024-05-18 13:57:54 +0800

    correct blur backdrop tailwind's backdrop-blur is not working on mobile. custom css class was added to temporarily solve this issue. should be revered once tailwind fixes this issue

--- app/components/skills.tsx
+++ app/components/skills.tsx
@@ -4,7 +4,7 @@ import { skills } from "../contents";
 export default function Skills() {
   return (
     <section id="skills" className="mb-16">
-      <div className="sticky top-0 z-20 backdrop-blur dark:bg-slate-900/75 bg-slate-200/75 py-4 -mx-10 lg:-mx-24 md:-mx-24 px-10 lg:px-24 md:px-24">
+      <div className="sticky top-0 z-20 custom-backdrop dark:bg-slate-900/75 bg-slate-200/75 py-4 -mx-10 lg:-mx-24 md:-mx-24 px-10 lg:px-24 md:px-24">
         <h2 className="dark:text-slate-200 text-slate-600 font-bold">SKILLS</h2>
       </div>
       <div className="grid gap-3 lg:gap-6 md:gap-4 lg:grid-cols-4 md:grid-cols-2 grid-cols-2 ">
